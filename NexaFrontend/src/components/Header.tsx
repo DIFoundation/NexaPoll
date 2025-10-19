94ba2cde51d975b9a7f3a11a9e7442cc90317d43
DIFoundation <adeniranibrahim165@gmail.com>
2025-10-19 15:14:58 +0100

    OKKK

--- NexaFrontend/src/components/Header.tsx
+++ NexaFrontend/src/components/Header.tsx
@@ -1,9 +1,39 @@
+'use client'
+import { AppKitConnectButton } from '@reown/appkit/react'
 import React from 'react'
 
 function Header() {
   return (
     <div>
-      <appkit-button />
+      <div>
+        <p>1</p>
+        <appkit-button />
+      </div>
+
+      <div>
+        <p>2</p>
+        <appkit-modal />
+      </div>
+
+      <div>
+        <p>3</p>
+        <appkit-connect-button />
+      </div>
+
+      <div>
+        <p>4</p>
+        <appkit-network-button />
+      </div>
+
+      <div>
+        <p>5</p>
+        <appkit-account-button />
+      </div>
+
+      <div>
+        <p>6</p>
+        <AppKitConnectButton />
+      </div>
     </div>
   )
 }
